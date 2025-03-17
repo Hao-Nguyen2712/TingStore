4774762c71549b519bf77ee6be9eb558e3f4a1c8
DuongTanDat <datdtce171751@fpt.edu.vn>
2025-03-17 13:12:25 +0700

    feat(user): Display product list and details on User pages

--- TingStore/Frontend/TingStore.Client/Areas/User/Models/Products/ProductResponse.cs
+++ TingStore/Frontend/TingStore.Client/Areas/User/Models/Products/ProductResponse.cs
@@ -1,9 +1,26 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace TingStore.Client.Areas.User.Models.Product
+using Product.Core.Models;
+
+namespace TingStore.Client.Areas.User.Models.Products
 {
     public class ProductResponse
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string ProductImage { get; set; }
+        public string Description { get; set; }
+        public string CategoryId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public List<ProductImage> Images { get; set; }
+    }
+    public class ProductImage
+    {
+        public string Url { get; set; } 
     }
 }
