dce097c623977d4a64c91a171e165af2a7cda5c6
DuongTanDat <datdtce171751@fpt.edu.vn>
2025-03-25 18:47:49 +0700

    fix: layout and backend

---
+++ TingStore/Frontend/TingStore.Client/Areas/Admin/Models/Dashboard/DashboardVM.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using TingStore.Client.Areas.Admin.Models.Order;
+
+namespace TingStore.Client.Areas.Admin.Models.Dashboard
+{
+    public class DashboardVM
+    {
+        public int TotalOrders { get; set; }
+        public int TotalUsers { get; set; }
+        public IEnumerable<OrderResponse> Orders { get; set; }
+    }
+}
